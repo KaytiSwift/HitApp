6a929ada1dcdb55dc302834c8f22f7b02412f18d
KaytiSwift <kaytiswift@yahoo.com>
2018-11-16 13:55:19 -0500

    fixed display names for expense view

--- HitApp/Models/Expense.cs
+++ HitApp/Models/Expense.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace HitApp.Models
     public class Expense
     {
         public int ExpenseId { get; set; }
+        [Display(Name = "Name")]
         public string ExpenseName { get; set; }
+        [Display(Name = "Total Cost")]
         public double ExpenseTotalCost { get; set; }
+        [Display(Name = "Notes")]
         public string ExpenseNotes { get; set; }
     }
 }
