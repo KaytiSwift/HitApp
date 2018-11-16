d396971112a65489a5f7f335a346020018d3cf14
mreilly29 <mreilly18@jcu.edu>
2018-11-16 13:40:14 -0500

    Put display names in Project Model

--- HitApp/Models/Project.cs
+++ HitApp/Models/Project.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,23 @@ namespace HitApp.Models
     public class Project
     {
         public int ProjectId { get; set; }
+
+        [DisplayName("Project Name")]
         public string ProjectName { get; set; }
+
+        [DisplayName("Start Date")]
         public DateTime ProjectStartDate { get; set; }
+
+        [DisplayName("End Date")]
         public DateTime ProjectEndDate { get; set; }
+
+        [DisplayName("Project Description")]
         public string ProjectDescription { get; set; }
+
+        [DisplayName("Contractor Info")]
         public string ProjectContractorInfo { get; set; }
+
+        [DisplayName("Total Budget")]
         public double ProjectTotalBudget { get; set; }
     }
 }
