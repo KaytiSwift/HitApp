7a2e4b9a8772d74775d806bae057f92a51be2592
andreyborovkov <andrey_borovkov@yahoo.com>
2018-11-26 14:13:39 -0500

    update, create, delete project

---
+++ HitApp/Data/Migrations/20181126190930_ChangedProjectEndDateToNullable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HitApp.Data.Migrations
+{
+    public partial class ChangedProjectEndDateToNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ProjectEndDate",
+                table: "Projects",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ProjectEndDate",
+                table: "Projects",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
